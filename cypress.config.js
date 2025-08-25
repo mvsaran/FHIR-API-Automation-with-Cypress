f857d9f3d93891e21334dba9264ed0072a22ef82
Saran Kumar <86158213+mvsaran@users.noreply.github.com>
2025-08-25 14:15:34 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",   // reports folder
+    overwrite: false,               // don’t overwrite old results
+    html: true,                     // generate HTML
+    json: true                      // generate JSON (for merging later)
+  },
   e2e: {
     baseUrl: "https://hapi.fhir.org/baseR4",
     setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config;
       // implement node event listeners here
     },
   },
